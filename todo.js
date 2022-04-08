ecf2d65974e0c10e3fa63fe2f924fd6211541fcf
Shubham Mahalkar <35593215+shubhammahalkar@users.noreply.github.com>
2022-04-08 23:26:57 +0530

    done todoapp

--- todo.js
+++ todo.js
@@ -11,8 +11,37 @@ removeButton.addEventListener('click',removeItem )
 
 
 function addItem(){
-    console.log('add ')
+   var input = document.getElementById('input')
+   var item = input.value
+   ul = document.getElementById('list')
+   var textnode = document.createTextNode(item)
+   
+   if (item === '') {
+       return false
+   }else{
+       li = document.createElement('li')
+       var checkbox = document.createElement('input')
+       checkbox.type = 'checkbox'
+       checkbox.setAttribute('id','check')
+
+       var label = document.createElement('label')
+       label.setAttribute('for','item')
+
+        ul.appendChild(label)
+        li.appendChild(checkbox)
+        label.appendChild(textnode)
+        li.appendChild(label)
+        ul.insertBefore(li, ul.childNodes[0])
+        
+        setTimeout(() => {
+             li.className = 'visual';
+        },2)
+
+        input.value = ''
+   }
 }
+
+
 function removeItem(){
  li = ul.children
  for (let index = 0; index < li.length; index++) {
