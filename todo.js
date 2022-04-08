cbdeb7085bc6d56a165af5ed332c055b016e3f36
Shubham Mahalkar <35593215+shubhammahalkar@users.noreply.github.com>
2022-04-08 22:58:14 +0530

    js start

--- todo.js
+++ todo.js
@@ -1,1 +1,25 @@
+var ul = document.getElementById('list')
+var li
+
+var addButton = document.getElementById('add')
+addButton.addEventListener('click',addItem)
+
+var removeButton = document.getElementById('remove')
+removeButton.addEventListener('click',removeItem )
+
+
+
+
+function addItem(){
+    console.log('add ')
+}
+function removeItem(){
+ li = ul.children
+ for (let index = 0; index < li.length; index++) {
+   while(li[index] && li[index].children[0].checked){
+        ul.removeChild(li[index])
+   }
+     
+ }
+}
  
